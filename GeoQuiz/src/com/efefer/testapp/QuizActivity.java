97367629fe7508d3e7ab6e25bcd57bf0988b165f
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2018-08-30 12:18:52 +0700

    First version of GeoQuiz

---
+++ GeoQuiz/src/com/efefer/testapp/QuizActivity.java
@@ -0,0 +1,41 @@
+package com.efefer.testapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+import android.view.Gravity;
+import android.view.View;
+
+public class QuizActivity extends Activity {
+
+  private Button mTrueButton;
+  private Button mFalseButton;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_quiz);
+
+    mTrueButton = (Button) findViewById(R.id.true_button);
+    mFalseButton = (Button) findViewById(R.id.false_button);
+
+    mTrueButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        Toast t = Toast.makeText(QuizActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT);
+        t.setGravity(Gravity.TOP, 0, 20);
+        t.show();
+      }
+    });
+
+    mFalseButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        Toast.makeText(QuizActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
+      }
+    });
+
+  }
+
+}
